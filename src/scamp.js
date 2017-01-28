4215001dddcc120615af3ff79b36beedc251ca79
Jay Gradon <jaygradon@outlook.com>
2017-01-28 14:35:46 +1300

    Added "ping" command

--- src/scamp.js
+++ src/scamp.js
@@ -1,1 +1,19 @@
-console.log('Scamp!');
+/* ScampBot for Discord
+ *  A ping pong bot, whenever you send "ping", it replies "pong".
+ */
+const config = require('./config.json');
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+  console.log('I am ready!');
+});
+
+bot.on('message', message => {
+  if (message.content === 'ping') {
+    message.channel.sendMessage('pong');
+  }
+});
+
+bot.login(config.token);
